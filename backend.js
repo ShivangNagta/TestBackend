ba44b8c2d7e1a04697b6395434d4c78acf0156b3
Shivang Nagta <shivangnag@gmail.com>
2024-11-11 22:59:27 +0530

    oo my goddo

---
+++ backend.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const PORT = 3000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        'message' : "successful"
+    })
+})
+
+app.post('/api', (req, res) => {
+    const { token } = req.body
+    res.json({
+        "message" : "alert has been sent"
+    })
+})
+
+app.listen(3000)
